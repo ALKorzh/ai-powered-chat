463ac2b90a3d990e522ca9023ea41489c4b28d9f
Victar Nikitsin <nikivictor64@gmail.com>
2025-05-11 14:10:47 +0300

    Add sending messages and logging out

--- Frontend/src/components/ChatItem/ChatItem.jsx
+++ Frontend/src/components/ChatItem/ChatItem.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import classes from './ChatItem.module.css'
 import MessageInput from "../MessageInput/MessageInput.jsx";
 
-const ChatItem = ({isVisible, setIsRecordActive, isRecordActive}) => {
+const ChatItem = ({isVisible, setIsRecordActive, isRecordActive, chatId, userId}) => {
     return (
         <div className={isVisible ? `${classes.showChatList} ${classes.chatItem}` : `${classes.hideChatList} ${classes.chatItem}`}>
             <p>ChatItem</p>
-            <MessageInput isActive={isRecordActive} setIsActive={setIsRecordActive}/>
+            <MessageInput isActive={isRecordActive} setIsActive={setIsRecordActive} chatId={chatId} userId={userId}/>
         </div>
     );
 };
